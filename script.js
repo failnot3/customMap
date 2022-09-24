bff87c910e9b36db1cf4e02ea5adc2b9cf1a0106
theDTS1 <orel1one@gmail.com>
2022-09-24 13:26:13 +0300

    verOne

---
+++ script.js
@@ -0,0 +1,34 @@
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: 42.66264, lng: 23.317780084658974 };
+    const mark = { lat: 42.64605630153414, lng: 23.339796895892906 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 11,
+        center: uluru,
+        mapId: "b26acea981ff127b"
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+        position: mark,
+        map: map,
+        icon:{
+            url:"green-box.svg",
+            scaledSize: new google.maps.Size(40, 40)
+        }
+    });
+    const markerOne = new google.maps.Marker({
+        position: { lat: 42.66212822871365, lng: 23.31805707634206 },
+        map: map,
+        icon:{
+            url:"green-box.svg",
+            scaledSize: new google.maps.Size(40, 40)
+        }
+    });
+}
+
+window.initMap = initMap;
+//42.662645514452144, 23.317780084658974
+// new avenue 42.64605630153414, 23.339796895892906
+// puzl 42.66212822871365, 23.31805707634206
